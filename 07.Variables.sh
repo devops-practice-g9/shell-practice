e650905daafa09c9251a8d44fa2d803c6def4d2b
mahesh <mahesh050917@gmail.com>
2026-02-01 12:07:17 +0530

    variables.sh v4

---
+++ 07.Variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Variables passed to the Script: $@"
+echo "Number of Variables passed to the Script: $#"
+echo "Script Name: $0"
+echo "Taking all arguments as one: $*"
+echo "who is running the script: $USER"
+echo "Home Directory of the user: $HOME"
+echo "PID of the Script: $$"
+sleep
+echo "Background process ID: $!"
+echo "Present working Directory: $PWD"
