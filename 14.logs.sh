ea5134b791793b20c8c16582d5b353505a12cc06
mahesh <mahesh050917@gmail.com>
2026-02-01 15:23:54 +0530

    logs script

---
+++ 14.logs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+Userid=$(id -u)
+LOG_DIR=/var/log/Shell_Script
+LOG_FILE=/var/log/Shell_Script/$0.log
+
+if [ $Userid -ne 0 ]; then
+  {
+    echo "Userid is $Userid"
+    echo "Please run the script with sudo root access"
+    exit 1
+  }
+fi
+
+mkdir -p $LOG_DIR
+
+VALIDATE() {
+   if [ $? -eq 0 ]; then
+      echo "$2....Success"
+    else
+      echo "$2...Failure"
+fi
+}
+
+
+dnf install nginxx -y &>> $LOG_FILE
+VALIDATE $? "Installing Nginx"
+
+dnf install htop -y  &>> $LOG_FILE
+VALIDATE $? "Installing htop"
+
+dnf install nodejs -y &>> $LOG_FILE
+VALIDATE $? "Installing nodejs"
