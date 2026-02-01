6061f639282de04b2b5e8764852547f49775a38b
mahesh <mahesh050917@gmail.com>
2026-02-01 11:37:39 +0530

    variables.sh v1

---
+++ 04.Variables.sh
@@ -0,0 +1,7 @@
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: $PERSON2 How are you ?"
+echo "$PERSON2:: $PERSON1 I am gud"
+echo "$PERSON1:: what are you doing"
+echo "$PERSON1:: $PERSON2 I am working on shell script"
