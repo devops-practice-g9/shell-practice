012a77067cd8f14f15ed59e31f2dce24b50d4d4b
mahesh <mahesh050917@gmail.com>
2026-02-01 12:35:48 +0530

    conditions v1

---
+++ 10.Conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NUM=$1
+
+if [ $NUM -gt 50 ]; then
+ 
+   echo "$NUM is greater than 50"
+
+   elif [ $NUM -eq 20 ]; then
+
+   echo "$NUM is equal to 50"
+  else
+   echo "$NUM is less than 50"
+fi
